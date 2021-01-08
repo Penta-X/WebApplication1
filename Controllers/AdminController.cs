d1beec6bbfc4b6b2ce51f20729ca7d8ee01eda6e
pybll <56275782+pybll@users.noreply.github.com>
2021-01-08 23:59:37 +0300

    08.01.2021

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -10,11 +10,45 @@ namespace WebApplication1.Controllers
     {
         // GET: Admin
         Context c = new Context();
-
+        [Authorize]
         public ActionResult Index()
         {
             var values = c.Blogs.ToList();
             return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteBlog(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetBlog(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("GetBlog", bl);
+        }
+        public ActionResult BlogUpdate(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.content = b.content;
+            blg.title = b.title;
+            blg.image = b.image;
+            blg.date = b.date;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
